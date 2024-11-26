3cc7068c3972ed4c9b947b7b90030df2bbabd0c8
ygs <ygs3004@naver.com>
2024-11-27 00:53:13 +0900

    백엔드 통신 디자인 패턴 - long polling

---
+++ long-polling/long-polling.js
@@ -0,0 +1,35 @@
+const app = require("express")();
+const jobs = {};
+
+app.post("/submit", (req, res) => {
+    const jobId = `job:${Date.now()}`;
+    jobs[jobId] = 0;
+    updateJob(jobId, 0);
+    res.end("\n\n" + jobId + "\n\n");;
+})
+
+app.get("/checkstatus", async (req, res) => {
+    console.log(jobs[req.query.jobId]);
+
+    while(await checkJobComplete(req.query.jobId) === false);
+    res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n");
+})
+
+const checkJobComplete = async (jobId) => {
+    return new Promise( (resolve, reject) => {
+        if(jobs[jobId] < 100){
+            setTimeout(() => resolve(false), 1000);
+        }else{
+            resolve(true);
+        }
+    })
+}
+
+const updateJob = (jobId, progress) => {
+    jobs[jobId] = progress;
+    console.log(`update ${jobId} to ${progress}`);
+    if(progress === 100) return;
+    setTimeout(() => updateJob(jobId, progress + 10), 3000);
+}
+
+app.listen(8080, () => console.log("listening on 8080"));
